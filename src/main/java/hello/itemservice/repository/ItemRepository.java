592a0ac22badbd6a9d62c626ef27efc9e064885a
JJoSuk <loveyou55042@gmail.com>
2023-06-17 23:51:30 +0900

    itemService_db

---
+++ src/main/java/hello/itemservice/repository/ItemRepository.java
@@ -0,0 +1,19 @@
+package hello.itemservice.repository;
+
+import hello.itemservice.domain.Item;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ItemRepository {
+
+    Item save(Item item);
+
+    void update(Long itemId, ItemUpdateDto updateParam);
+
+    Optional<Item> findById(Long id);
+
+    //                    검색 조건
+    List<Item> findAll(ItemSearchCond cond);
+
+}
